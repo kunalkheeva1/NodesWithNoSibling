4477d33e1a9ab0df018bef4365475d185cb30526
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-11 21:16:10 -0700

    coded the actual method where it is actually going to take place.

--- src/NodesWithNoSibling.java
+++ src/NodesWithNoSibling.java
@@ -1,5 +1,18 @@
+import java.util.*;
+
 public class NodesWithNoSibling {
+   static ArrayList<Integer> nodesWithNoSibling(int node){
+       ArrayList<Integer> ans = new ArrayList<>();
+       rec(node, ans);
+       if(ans.size()==0){
+           ans.add(-1);
+           return ans;
+       }else{
+           Collections.sort(ans);
+           return ans;
+       }
+   }
     public static void main(String[] args) {
-        
+
     }
 }
