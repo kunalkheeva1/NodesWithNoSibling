3f51e3fb5f4334134ca1310a5d29b11bea48597d
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-11 21:23:56 -0700

    added the comments for clarity

--- src/NodesWithNoSibling.java
+++ src/NodesWithNoSibling.java
@@ -1,4 +1,5 @@
 import java.util.*;
+//node class
 class Node{
     int data;
     Node right;
@@ -13,17 +14,21 @@ class Node{
 
 public class NodesWithNoSibling {
 
-
+    //recursive call for the data
     static void rec(Node root, ArrayList<Integer>ans){
+        //if null then return
         if(root ==null){
             return;
         }
+        //if right is not null, then store its data
         if(root.left ==null && root.right != null){
             ans.add(root.right.data);
         }
+        //if left is not null , then store its data
         if(root.left != null && root.right ==null){
             ans.add(root.left.data);
         }
+        //call for left and right otherwise
         rec(root.left, ans);
         rec(root.right,ans);
     }
@@ -31,13 +36,13 @@ public class NodesWithNoSibling {
    static ArrayList<Integer> nodesWithNoSibling(Node node){
        ArrayList<Integer> ans = new ArrayList<>();
        rec(node, ans);
+       //if empty then print -1, otherwise sort the list and return it
        if(ans.size()==0){
            ans.add(-1);
-           return ans;
        }else{
            Collections.sort(ans);
-           return ans;
        }
+       return ans;
    }
     public static void main(String[] args) {
 
